49fc45581c74bd8033f823ab91201ea2b08dc0c6
Jesus Ramirez <jesusdevelp@gmail.com>
2020-03-10 18:29:16 -0600

    We connect the views to redux

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { getUsers } from "../actions";
 
 export class Home extends Component {
+  componentWillMount() {
+    this.props.getUsers();
+  }
   render() {
     return (
       <div>
@@ -10,4 +16,19 @@ export class Home extends Component {
   }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return {
+    users: state.getUsers
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      getUsers
+    },
+    dispatch
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
