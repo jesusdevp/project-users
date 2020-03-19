f70ce4bb146e3c6d214e3009f1881621ee39c13c
Jesus Ramirez <jesusdevelp@gmail.com>
2020-03-19 14:02:29 -0600

    stylizing cards

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -9,12 +9,15 @@ class UserItem extends Component {
       <div className="card">
         <div className="card-content">
           <div className="UserItem-leftBox">
-            <img src="" alt="profile" />
+            <img
+              className="UserItem-image"
+              src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+              alt="profile"
+            />
           </div>
           <div className="UserItem-rightBox">
-            <h3 className="UserItem-id">{id}</h3>
-            <h2 className="UserItem-name">{name}</h2>
-            <h3 className="UserItem-last_name">{lastName}</h3>
+            {/* <h3 className="UserItem-id">{id}</h3> */}
+            <h2 className="UserItem-name">{name + " " + lastName}</h2>
             <h3 className="UserItem-facebook">{facebook}</h3>
           </div>
         </div>
